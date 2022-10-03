e2d73452885f3e1129d99230974a49cb5b632e72
toanhminh0412 <toanhminh0412@gmail.com>
2022-10-02 22:58:36 -0700

    Attempt to display Markdown properly

---
+++ business_project/static/js/command_set/detail_page.js
@@ -0,0 +1,7 @@
+/*
+const commandsMD = $("#commands-display").attr("data-commands");
+console.log(commandsMD);
+console.log("Hello");
+console.log(document.querySelector("#commands-display"));
+document.querySelector("#commands-display").mdContent = commandsMD;
+*/
