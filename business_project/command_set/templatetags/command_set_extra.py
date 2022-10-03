450aa03d7289b903b3a66ca156af29bde0c2b003
toanhminh0412 <toanhminh0412@gmail.com>
2022-10-02 22:56:22 -0700

    Add a comment

--- business_project/command_set/templatetags/command_set_extra.py
+++ business_project/command_set/templatetags/command_set_extra.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+# Custom filter: remove quotation marks from strings in templates
 @register.filter
 def strip_double_quotes(quoted_string):
     return quoted_string.replace("'", '')
